ec0d8d7592f88301d9626eb9fae0b3cfe12a352f
justin-chu <30902118+justin-chu@users.noreply.github.com>
2018-04-11 15:34:04 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,7 +13,19 @@ client.on('message', message => {
 
 client.on('message', message => {
     if(message.content.toLowerCase().substring(0, 10) === 'i love you') {
-            message.reply('ya one sorreh fuckin twat arent ya mate ');
+            message.reply('ya one sorreh fackin twat arent ya mate ');
+    }
+});
+
+client.on('message', message => {
+    if(message.content.toLowerCase().substring(0, 5) === '!play') {
+            message.reply('ya sorry cunt, only the lorde scumbag can play musics  ');
+    }
+});
+
+client.on('message', message => {
+    if(message.attachments) {
+            message.reply('is that a picture of you mate? golly fuckin eh you are uglier than i thought ya fackin prick');
     }
 });
 
